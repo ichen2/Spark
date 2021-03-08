f670005959d05507760ada8f8c73421b2ef396e2
Ian Chen <ichen2@lsu.edu>
2021-03-07 21:31:06 -0600

    Added spotify authentication and basic player

--- app/src/main/java/com/example/spark/MainActivity.java
+++ app/src/main/java/com/example/spark/MainActivity.java
@@ -3,12 +3,91 @@ package com.example.spark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.Image;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "343c7425946a4130b0aed8bd56091458";
+    private static final String REDIRECT_URI = "spark://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    private TextView mSongNameText;
+    private ImageView mAlbumCoverImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSongNameText = (TextView) findViewById(R.id.tv_song_name);
+        mAlbumCoverImage = (ImageView) findViewById(R.id.iv_album_cover);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Set the connection parameters
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    private void connected() {
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1E4zUdHmpg9jpG");
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                        mSongNameText.setText(track.name);
+                        mSpotifyAppRemote
+                                .getImagesApi()
+                                .getImage(playerState.track.imageUri, Image.Dimension.LARGE)
+                                .setResultCallback(
+                                        bitmap -> {
+                                            mAlbumCoverImage.setImageBitmap(bitmap);
+                                        });
+                        //Glide.with(this).load(track.imageUri).into(mAlbumCoverImage);
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 }
